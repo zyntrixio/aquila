75819f123723c9b0198ee9b7d4fbfe7c6972269a
Chris Pressland <mail@cpressland.io>
2024-04-18 16:05:01 +0100

    Complete upgrade to Python 3.12 (#48)
    * Complete upgrade to Python 3.12
    
    * More Fixes

--- tests/unit/test_template_loader.py
+++ tests/unit/test_template_loader.py
@@ -5,6 +5,6 @@ from aquila.blob_storage import template_loader
 
 def test_template_loader_no_fetch(mocker: MockerFixture) -> None:
     mock_logger = mocker.patch.object(template_loader, "logger")
-    assert template_loader.dont_fetch_templates is True
+    assert template_loader.dont_fetch_templates
     assert template_loader.get_template("any", "any") is None
     mock_logger.debug.assert_called_with("TESTING set to %s, returning None", True)
